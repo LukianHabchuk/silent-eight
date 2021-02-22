822c894c626f7ac12bf66f71cd710dbbd558b81f
Lukian Hrabchuk <lukian359@gmail.com>
2021-02-22 11:01:12 +0100

    path to files with names tested

--- src/test/java/com/homework/silenteight/SilentEightApplicationTests.java
+++ src/test/java/com/homework/silenteight/SilentEightApplicationTests.java
@@ -3,11 +3,23 @@ package com.homework.silenteight;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SilentEightApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void shouldFindFileWithMaleNames() {
+		File file = new File("src/main/resources/genderNames/male.txt");
+		assertEquals("male.txt", file.getName());
+	}
+
+	@Test
+	void shouldFindFileWithFemaleNames() {
+		File file = new File("src/main/resources/genderNames/female.txt");
+		assertEquals("female.txt", file.getName());
 	}
 
 }
