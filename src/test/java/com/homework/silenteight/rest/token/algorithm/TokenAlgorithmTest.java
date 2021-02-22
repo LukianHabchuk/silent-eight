c12aed4ba4b11f5a53c10539066bf4d1ca10f710
Lukian Hrabchuk <lukian359@gmail.com>
2021-02-22 15:11:54 +0100

    tested an algorithm for obtaining tokens

---
+++ src/test/java/com/homework/silenteight/rest/token/algorithm/TokenAlgorithmTest.java
@@ -0,0 +1,27 @@
+package com.homework.silenteight.rest.token.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenAlgorithmTest {
+
+    private TokenAlgorithm algorithm = new TokenAlgorithm();
+    private static int FEMALE_FILE_SIZE = 4955;
+    private static int MALE_FILE_SIZE = 2943;
+
+    @Test
+    void getAllTokens() {
+        assertEquals(algorithm.getAllTokens().size(), FEMALE_FILE_SIZE + MALE_FILE_SIZE);
+    }
+
+    @Test
+    void getMale() {
+        assertEquals(algorithm.getMale().size(), MALE_FILE_SIZE);
+    }
+
+    @Test
+    void getFemale() {
+        assertEquals(algorithm.getFemale().size(), FEMALE_FILE_SIZE);
+    }
+}
