3a62b086ecfd184c9fab581d887adf8fbb28049a
Lukian Hrabchuk <lukian359@gmail.com>
2021-02-22 10:51:33 +0100

    Initial commit

---
+++ src/test/java/com/homework/silenteight/SilentEightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.silenteight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilentEightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
